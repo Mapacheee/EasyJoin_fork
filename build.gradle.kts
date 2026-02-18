603ce4036ca0857c0bd6723451169e73b2985640
Benjamín Isasmendi C <bisasmendic@gmail.com>
2026-02-17 23:04:12 -0400

    refactor build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,11 +25,14 @@ repositories {
     maven("https://repo.codemc.io/repository/maven-public/")
 }
 
+
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
 
     api("com.thewinterframework:paper:1.0.4") {
         exclude(group = "io.papermc.paper", module = "paper-api")
+        exclude(group = "com.google.guava", module = "guava")
+        exclude(group = "com.google.code.gson", module = "gson")
     }
     annotationProcessor("com.thewinterframework:paper:1.0.4")
 
@@ -49,20 +52,61 @@ dependencies {
 
     api("org.incendo:cloud-paper:2.0.0-beta.10")
     api("org.incendo:cloud-annotations:2.0.0")
-
     compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly("fr.xephi:authme:5.6.0-SNAPSHOT")
     compileOnly("com.nickuc.login:nlogin-api:10.0")
 
+    implementation("com.zaxxer:HikariCP:7.0.2")
     implementation("net.kyori:adventure-text-serializer-legacy:4.26.1")
     implementation("net.kyori:adventure-text-minimessage:4.26.1")
-    implementation("com.zaxxer:HikariCP:7.0.2")
 }
 
 
 tasks.shadowJar {
-    relocate("com.zaxxer.hikari", "me.espryth.easyjoin.libs.hikari")
+    minimize {
+        exclude(dependency("com.thewinterframework:.*"))
+        exclude(dependency("com.google.inject:.*"))
+        exclude(dependency("org.incendo:.*"))
+    }
+
+    relocate("com.thewinterframework", "me.espryth.easyjoin.libs.winter")
+    relocate("com.google.inject", "me.espryth.easyjoin.libs.guice")
+    relocate("org.incendo.cloud", "me.espryth.easyjoin.libs.cloud")
+    relocate("javax.inject", "me.espryth.easyjoin.libs.inject")
+    relocate("aopalliance", "me.espryth.easyjoin.libs.aopalliance")
+    relocate("io.leangen.geantyref", "me.espryth.easyjoin.libs.geantyref")
+
+    exclude("org/checkerframework/**")
+    exclude("META-INF/maven/org.checkerframework/**")
+    exclude("com/google/errorprone/**")
+    exclude("com/google/j2objc/**")
+
+    exclude("META-INF/MANIFEST.MF")
+    exclude("META-INF/*.SF")
+    exclude("META-INF/*.DSA")
+    exclude("META-INF/*.RSA")
+    exclude("META-INF/versions/**")
+    exclude("META-INF/maven/**")
+    exclude("META-INF/proguard/**")
+    exclude("META-INF/native-image/**")
+    exclude("module-info.class")
+    exclude("**/module-info.class")
+    exclude("META-INF/*.kotlin_module")
+
     archiveClassifier.set("")
+
+    mergeServiceFiles()
+
+    manifest {
+        attributes(
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version,
+            "Multi-Release" to "false"
+        )
+    }
+
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
 }
 
 tasks.build {
